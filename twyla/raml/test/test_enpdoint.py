8e7d6323c56d4bcc1b308487be0468208010647d
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2017-09-04 16:34:07 +0200

    Fixed indentation

--- twyla/raml/test/test_enpdoint.py
+++ twyla/raml/test/test_enpdoint.py
@@ -1,5 +1,5 @@
 from twyla.raml import Endpoint
 
 def test_display_name_not_specified():
-  ep = Endpoint('/status', {'get': {}})
-  assert ep.display_name == '/status'
+    ep = Endpoint('/status', {'get': {}})
+    assert ep.display_name == '/status'
