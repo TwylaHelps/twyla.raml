0ca999f3cf033d676f8f4ac5d68fadad264b1756
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2017-09-06 20:22:41 +0200

    Working on data types

--- twyla/raml/data_types.py
+++ twyla/raml/data_types.py
@@ -9,8 +9,12 @@ def for_type(type_name):
 class DataType:
 
     def __init__(self, spec):
+        self.required = spec.pop('required', True)
         self.spec = spec
         self.parse_spec()
+
+    def parse_spec(self):
+        raise NotImplementedError()
 
     @classmethod
     def from_spec(cls, spec):
@@ -22,6 +26,9 @@ class StringType(DataType):
 
     def parse_spec(self):
         pass
+
+    def validate(self, value):
+        return isinstance(value, str)
 
 
 @for_type('object')
--- twyla/raml/test/test_data_type.py
+++ twyla/raml/test/test_data_type.py
@@ -3,3 +3,25 @@ from twyla.raml.data_types import DataType, StringType
 def test_creates_subset_by_key():
     string_type = DataType.from_spec({'type': 'string'})
     assert isinstance(string_type, StringType)
+
+
+def test_required():
+    string_type = DataType.from_spec({'type': 'string', 'required': False})
+    assert not string_type.required
+
+
+def test_required_default_true():
+    string_type = DataType.from_spec({'type': 'string'})
+    assert string_type.required
+
+
+def test_object_tree():
+    spec = {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'email': {'type': 'string',
+                      'required': False}
+        }
+    }
+    object_type = DataType.from_spec(spec)
